dc6876547f959fdba8d2a8f9ece69a639e7beb1a
hellozjf <908686171@qq.com>
2025-10-06 19:41:41 +0800

    定义 Destinations，其中遇到了 Icons 找不到的问题，原来是 compose bom 从 2024.9 升级到 2025.9 之后，不再默认包含 material-icons-extended 了，需要自己手动导入

---
+++ app/src/main/java/cn/hellozjf/project/composequiz/Destinations.kt
@@ -0,0 +1,17 @@
+package cn.hellozjf.project.composequiz
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class Destinations(
+  val label: String,
+  val contentDescription: String,
+  val icon: ImageVector
+) {
+  HOME("Home", "Home Screen", Icons.Default.Home),
+  CONTACTS("Contacts", "Contects screen", Icons.Default.Face),
+  FAVORITES("Favorites", "Favorites screen", Icons.Default.Favorite)
+}
