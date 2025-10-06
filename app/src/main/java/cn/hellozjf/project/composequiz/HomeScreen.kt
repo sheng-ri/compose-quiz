92fa85574ecdaa975c038cb30a8862c3f69b31e7
hellozjf <908686171@qq.com>
2025-10-06 19:45:44 +0800

    添加三个 Screen

---
+++ app/src/main/java/cn/hellozjf/project/composequiz/HomeScreen.kt
@@ -0,0 +1,30 @@
+package cn.hellozjf.project.composequiz
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun HomeScreen(
+  modifier: Modifier = Modifier,
+  icon: ImageVector,
+  contentDescription: String
+) {
+  Box(
+    modifier = modifier.fillMaxSize()
+  ) {
+    Icon(
+      imageVector = icon,
+      contentDescription = contentDescription,
+      tint = Color.Blue,
+      modifier = Modifier
+        .align(Alignment.Center)
+        .fillMaxSize()
+    )
+  }
+}
