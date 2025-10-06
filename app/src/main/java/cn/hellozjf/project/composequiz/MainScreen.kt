729a1d18109c82506aab89f570bf9c83c767dabe
hellozjf <908686171@qq.com>
2025-10-06 19:18:11 +0800

    kotlin、compose、navigation3都使用最新版本，然后编译就不报错了，也是挺神奇的

--- app/src/main/java/cn/hellozjf/project/composequiz/MainScreen.kt
+++ app/src/main/java/cn/hellozjf/project/composequiz/MainScreen.kt
@@ -3,7 +3,6 @@ package cn.hellozjf.project.composequiz
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.NavKey
-import androidx.navigation3.runtime.entry
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
