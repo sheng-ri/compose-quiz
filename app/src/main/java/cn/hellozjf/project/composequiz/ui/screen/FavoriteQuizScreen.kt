42d52b5d2715e889d88a8f37dddf581b687daee7
hellozjf <908686171@qq.com>
2025-10-11 19:38:29 +0800

    收藏的问题列表的问题，最后面加一个三角形，代表是否展示详细内容

--- app/src/main/java/cn/hellozjf/project/composequiz/ui/screen/FavoriteQuizScreen.kt
+++ app/src/main/java/cn/hellozjf/project/composequiz/ui/screen/FavoriteQuizScreen.kt
@@ -8,21 +8,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation3.runtime.NavKey
@@ -66,6 +71,7 @@ fun FavoriteQuizScreen(
     }
   }
   val listState = rememberLazyListState()
+  var questionExpandMap = remember { mutableStateMapOf<Int, Boolean>() }
 
   Column(
     modifier = modifier.fillMaxSize()
@@ -88,9 +94,20 @@ fun FavoriteQuizScreen(
       if (quizList.isNotEmpty()) {
         quizList.forEachIndexed { index, quiz ->
           item(key = quiz.id) {
-            Text(
-              text = quiz.question
-            )
+            val expand = questionExpandMap[quiz.id] ?: false
+            Row(
+              verticalAlignment = Alignment.CenterVertically
+            ) {
+              Text(
+                text = quiz.question,
+                modifier = Modifier.weight(1f)
+              )
+              Icon(
+                imageVector = Icons.Filled.ArrowDropDown,
+                contentDescription = null,
+                modifier.rotate(if (expand) 180f else 0f)
+              )
+            }
           }
         }
       }
