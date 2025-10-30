1e9bfe626155890007eb7327b1dc1fb212e0d4be
hellozjf <908686171@qq.com>
2025-10-30 20:56:47 +0800

    如果标题太长，显示3个点，点击标题之后弹出气泡显示完整标题

--- app/src/main/java/cn/hellozjf/project/composequiz/ui/component/MyTopAppBar.kt
+++ app/src/main/java/cn/hellozjf/project/composequiz/ui/component/MyTopAppBar.kt
@@ -7,13 +7,18 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import cn.hellozjf.project.composequiz.R
 import kotlinx.coroutines.launch
 
@@ -26,13 +31,31 @@ fun MyTopAppBar(
 ) {
 
   val coroutineScope = rememberCoroutineScope()
+  val tooltipState = rememberTooltipState()
 
   TopAppBar(
     title = {
-      Text(
-        text = title,
-        fontWeight = FontWeight.Bold
-      )
+      TooltipBox(
+        positionProvider = TooltipDefaults.rememberTooltipPositionProvider(),
+        tooltip = {
+          PlainTooltip {
+            Text(text = title)
+          }
+        },
+        state = tooltipState
+      ) {
+        Text(
+          text = title,
+          maxLines = 1,
+          overflow = TextOverflow.Ellipsis,
+          fontWeight = FontWeight.Bold,
+          modifier = Modifier.clickable {
+            coroutineScope.launch {
+              tooltipState.show()
+            }
+          }
+        )
+      }
     },
     navigationIcon = {
       IconButton(onClick = { /* 处理导航菜单点击 */ }) {
